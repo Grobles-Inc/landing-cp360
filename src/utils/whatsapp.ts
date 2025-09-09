032c7a2476b82de0c56118cbe8342012b82025d3
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2025-09-08 19:24:15 -0500

    Cambios FeedBack

--- src/utils/whatsapp.ts
+++ src/utils/whatsapp.ts
@@ -4,6 +4,11 @@ interface WhatsappMessage {
   mensaje: string;
 }
 
+interface PaqueteMessage {
+  paquete: string;
+  precio: string;
+}
+
 export const sendWhatsappMessage = async (message: WhatsappMessage) => {
   const { nombre, empresa, mensaje } = message;
   const formateadoMessage = `
@@ -13,6 +18,22 @@ export const sendWhatsappMessage = async (message: WhatsappMessage) => {
     *Empresa:* ${empresa}
     *Mensaje:* ${mensaje}
   `;
+  const phoneNumber = "51971419928";
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(formateadoMessage)}`;
+
+  window.open(whatsappUrl, '_blank');
+
+  return whatsappUrl;
+}
+
+export const sendPaqueteWhatsappMessage = async (message: PaqueteMessage) => {
+  const { paquete, precio } = message;
+  
+  const formateadoMessage = `
+    Hola, requiero más información sobre su plan ${paquete} (${precio}). 
+    Me gustaría recibir más detalles y una propuesta personalizada.
+  `;
+  
   const phoneNumber = "51971419928";
   const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(formateadoMessage)}`;
 
